15846468e1c808b05babb00b6822eebb2f5e2fda
Univindy <vincent-thiery@hotmail.fr>
2022-05-18 09:48:25 +0200

    Ajout historique & deux btn

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ var choixP1
 var choixP2
 var scorep1 = 0
 var scorep2 = 0
+var historique = [];
+var winner;
 
-/*
-* Lancement d'un test automatisé
-*/
-match(3);
 
 /*
 * Fonction Random qui retoune un choix entre Pierre Feuille & Ciseaux, 
@@ -23,53 +21,51 @@ function random(){
 /*
 * Fonction compare qui compare les choix des joueurs 1 et 2 et itere un point de score
 */
-function compare(choixp1, choixp2 ){
-    
-    console.log("Player 1: "+choixp1 + "  Player2: "+choixp2)
-    if(choixp1==choixp2){console.log("match nul")}
-    else{
-        if(choixp1 =="p"){
-            
-            if(choixp2=="c"){
-                console.log("Player 1 gagne")
+function compare(choixp1, choixp2) {
+        if (choixp1 == "p") {
+
+            if (choixp2 == "c") {
                 scorep1++
-            }
-            if(choixp2=="f"){
-                console.log("Player 2 gagne")
+                return "joueur 1"
+            } else if (choixp2 == "f") {
                 scorep2++
+                return "joueur 2"
+            } else {
+                return "aucun"
             }
 
         }
-       
-        if(choixp1 =="f"){
-            
-            if(choixp2=="c"){
-                console.log("Player 2 gagne")
+
+        if (choixp1 == "f") {
+
+            if (choixp2 == "c") {
                 scorep2++
-            }
-            if(choixp2=="p"){
-                console.log("Player 1 gagne")
+                return "joueur 2"
+            } else if (choixp2 == "p") {
                 scorep1++
+                return "joueur 1"
+            } else {
+                return "aucun"
+            }
+        }
+
+
+
+        if (choixp1 == "c") {
+
+            if (choixp2 == "p") {
+                scorep2++
+                return "joueur 2"
+            } else if (choixp2 == "f") {
+                scorep1++
+                return "joueur 1"
+            } else {
+                return "aucun"
             }
 
         }
-    
-        if(choixp1 =="c"){
-            
-            if(choixp2=="p"){
-                console.log("Player 2 gagne")
-                scorep2++
-            }
-            if(choixp2=="f"){
-                console.log("Player 1 gagne")
-                scorep1++
-            }
-
-        }
-       
-    }
-    
 }
+
 
 /*
 * Fonction lancer qui simule la procedure de lancement d'une partie
@@ -77,23 +73,40 @@ function compare(choixp1, choixp2 ){
 function lancer(){
     choixP2 = random()
     choixP1 = random()
-    compare(choixP1,choixP2)
+    winner = compare(choixP1,choixP2)
 }
 
 /*
 * Fonction match qui reinit le score et boucle le nombre de round de la partie
 */
 function match(nombrepartie) {
-        scorep1=0;
-        scorep2=0;
-    for(var i=0; i<nombrepartie;i++){
-        lancer()    
+    scorep1 = 0;
+    scorep2 = 0;
+    historique.push("--- Nouvelle partie de " + nombrepartie + " rounds ---")
+
+    for (var i = 0; i < nombrepartie; i++) {
+        lancer()
+        historique.push(i + 1 + " : P1 " + choixP1 + " / P2 " + choixP2 + " -  Vainqeur: " + winner)
     }
-    if(scorep1==0 && scorep2==0) {
-        console.log("MATCH NUL")
-    } 
-    console.log("SCORE:       "+"Player 1: "+scorep1 + "  Player2: "+scorep2)
+
+    historique.push("SCORE FINAL:       " + "Player 1: " + scorep1 + "  Player2: " + scorep2);
+
+    /* Affichage */
+    historique.forEach(element => {
+        console.log(element);
+    });
 }
+
+function ClearHistorique(){
+    historique = [];
+    console.log("Historique vide");
+}
+
+function Tests(){
+    console.log("TDD");
+}
+
+
 
 
 
